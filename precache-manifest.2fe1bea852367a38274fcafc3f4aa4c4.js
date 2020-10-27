aefa79b18fefe6991d85529aa71033079cfcabe2
zeeshan <zeeshansaaab@gmail.com>
2020-10-27 11:31:19 +0500

    Updates

---
+++ precache-manifest.2fe1bea852367a38274fcafc3f4aa4c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135d34fbae73320780c191e653c63845",
+    "url": "/Books/index.html"
+  },
+  {
+    "revision": "568a5746edfb1d8a4ef1",
+    "url": "/Books/static/css/main.64c337b8.chunk.css"
+  },
+  {
+    "revision": "a6c0b0465c279301cca8",
+    "url": "/Books/static/js/2.c1a3533a.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/Books/static/js/2.c1a3533a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568a5746edfb1d8a4ef1",
+    "url": "/Books/static/js/main.afb3b608.chunk.js"
+  },
+  {
+    "revision": "95fe25c151d85ab9fcdc",
+    "url": "/Books/static/js/runtime-main.92ade4f5.js"
+  },
+  {
+    "revision": "7ae4e597aea67b22fcf0e0c494845538",
+    "url": "/Books/static/media/logo.7ae4e597.png"
+  }
+]);
